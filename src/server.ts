365afc86316ed6eaf70bb3b24fd4b22573a99e02
Omar Alwahiby <omaralwahiby@Omars-MacBook-Pro.local>
2023-05-23 13:12:42 +0300

    fix server

--- src/server.ts
+++ src/server.ts
@@ -3,39 +3,39 @@ import fastifyAutoload from '@fastify/autoload';
 import fastifySensible from '@fastify/sensible';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from "@fastify/swagger-ui";
-import {  TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { join } from 'path';
 
 export const server = fastify({
   logger: true,
-  ajv:{
-    customOptions:{
-      removeAdditional:'all',
-      ownProperties:true,
+  ajv: {
+    customOptions: {
+      removeAdditional: 'all',
+      ownProperties: true,
     },
   }
 }).withTypeProvider<TypeBoxTypeProvider>();
 
 server.register(fastifySwagger, {
-  mode:'dynamic',
-  openapi:{
-    info:{
+  mode: 'dynamic',
+  openapi: {
+    info: {
       title: 'node template',
       description: 'Testing the Fastify swagger API',
       version: '0.1.0'
     },
     // security: [
-		// 	{
-		// 		bearerAuth: [],
-		// 	},
-		// ],
+    // 	{
+    // 		bearerAuth: [],
+    // 	},
+    // ],
     // components:{
     //   securitySchemes:{
     //     bearerAuth: {
-		// 			type: 'http',
-		// 			scheme: 'bearer',
-		// 			bearerFormat: 'JWT',
-		// 		},
+    // 			type: 'http',
+    // 			scheme: 'bearer',
+    // 			bearerFormat: 'JWT',
+    // 		},
     //   }
     // }
   }
@@ -46,21 +46,21 @@ const swaggerUiOptions = {
 };
 
 server.register(fastifySensible);
-server.register(fastifySwaggerUi,swaggerUiOptions);
+server.register(fastifySwaggerUi, swaggerUiOptions);
 server.register(fastifyAutoload, {
-	dir: join(__dirname, 'routes'),
+  dir: join(__dirname, 'routes'),
 });
 
 const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
 
 export function listen() {
-	server
-		.listen({
-			port: port,
-			host: '0.0.0.0',
-		})
-		.catch((err) => {
-			server.log.error(err);
-			process.exit(1);
-		});
+  server
+    .listen({
+      port: port,
+      host: '0.0.0.0',
+    })
+    .catch((err) => {
+      server.log.error(err);
+      process.exit(1);
+    });
 }
