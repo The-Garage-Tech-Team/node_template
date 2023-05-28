9f967d47e991a9ddc7e0004a022019e48e21da7c
sultan901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2023-05-28 18:51:58 +0300

    add Login & Signin

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,42 +4,63 @@ import { Users } from "../type-object/user-type";
 import * as bcrypt from "bcrypt";
 
 export const Register = async (
-    request: FastifyRequest,
-    reply: FastifyReply
+  request: FastifyRequest,
+  reply: FastifyReply
 ) => {
-    const user = request.body as Users;
-    const SALT = process.env.SALT;
+  const user = request.body as Users;
+  const SALT = process.env.SALT;
 
-    const hashedPassword = await bcrypt.hash(user.password, Number(SALT));
-    return await prismaClient.user.create({
-        data: {
-            ...user,
-            password: hashedPassword,
-        },
+  const hashedPassword = await bcrypt.hash(user.password, Number(SALT));
+  return await 
+   prismaClient.user
+    .create({
+      data: {
+        ...user,
+        password: hashedPassword,
+        email: user.email.toLocaleLowerCase(),
+      },
+    })
+    .then((User) => {
+      if (User.email === user.email) {
+        return "Email Already Registered";
+      } else return reply.send(User);
     });
 };
 export const Login = async (request: FastifyRequest, reply: FastifyReply) => {
-    const user = request.body as Users;
-
-    return await prismaClient.user
-        .findUnique({
-            where: {
-                email: user.email,
-            },
-        })
-
-        .then(async (User) => {
-            if (!User) {
-                reply.send("user not found")
-            }
-            const checkPassword = await bcrypt.compare(user.password, User.password);
-            if (checkPassword) {
-                return User;
-            } else return "Passwords do not match";
-        });
+  const user = request.body as Users;
+  return await prismaClient.user
+    .findUnique({
+      where: {
+        email: user.email,
+      },
+    })
+    .then(async (User) => {
+      const token = await reply.jwtSign(
+        {
+          id: User.user_id,
+        },
+        { expiresIn: "2h" }
+      );
+      const result = {
+        name: User.name,
+        id: User.user_id,
+        token,
+      };
+      if (!User) {
+        reply.send("user not found");
+      }
+      const checkPassword = await bcrypt.compare(user.password, User.password);
+      if (!checkPassword) {
+        reply.send("password incorrect");
+      }
+      if (!User.isActive) {
+        reply.send("user is not active");
+      }
+      reply.send(result);
+    });
 };
 
 export const getUser = async (request: FastifyRequest, reply: FastifyReply) => {
-    const user = await prismaClient.user.findMany();
-    return user;
+  const user = await prismaClient.user.findMany();
+  return user;
 };
