b1fd334fb043a750c48b6497209a55f981ded2f0
sultan901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2023-05-31 12:51:42 +0300

    add edite feature

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,74 +2,103 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { prismaClient } from "../prisma";
 import { Users } from "../type-object/user-type";
 import * as bcrypt from "bcrypt";
+import { userInfo } from "os";
 
+const SALT = process.env.SALT;
 export const Register = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
   const user = request.body as Users;
-  const SALT = process.env.SALT;
-
   const hashedPassword = await bcrypt.hash(user.password, Number(SALT));
-  return await prismaClient.user
-    .findUnique({
-      where: {
+  const users = await prismaClient.user.findUnique({
+    where: {
+      email: user.email.toLowerCase(),
+    },
+  });
+  if (users) {
+    reply.send("user already exists");
+  } else {
+    await prismaClient.user.create({
+      data: {
+        ...user,
+        password: hashedPassword,
         email: user.email.toLowerCase(),
       },
-    })
-    .then((users) => {
-      if (users) {
-        reply.send("user already exists");
-      } else {
-        return prismaClient.user
-          .create({
-            data: {
-              ...user,
-              password: hashedPassword,
-              email: user.email.toLowerCase(),
-            },
-          })
-          .then(() => {
-            reply.send("Registered");
-          });
-      }
     });
-}
+
+    reply.send("Registered");
+  }
+};
 export const Login = async (request: FastifyRequest, reply: FastifyReply) => {
   const user = request.body as Users;
-  return await prismaClient.user
-    .findUnique({
-      where: {
-        email: user.email,
-      },
-    })
-    .then(async (User) => {
-      const token = await reply.jwtSign(
-        {
-          id: User.user_id,
-        },
-        { expiresIn: "2h" }
-      );
-      const result = {
-        name: User.name,
-        id: User.user_id,
-        token,
-      };
-      if (!User) {
-        reply.send("user not found");
-      }
-      const checkPassword = await bcrypt.compare(user.password, User.password);
-      if (!checkPassword) {
-        reply.send("password incorrect");
-      }
-      if (!User.isActive) {
-        reply.send("user is not active");
-      }
-      reply.send(result);
-    });
+  const User = await prismaClient.user.findUnique({
+    where: {
+      email: user.email.toLowerCase(),
+    },
+  });
+
+  if (!User) {
+    reply.send("user not found");
+  }
+  const checkPassword = await bcrypt.compare(user.password, User.password);
+  if (!checkPassword) {
+    reply.send("password incorrect");
+  }
+  if (!User.isActive) {
+    reply.send("user is not active");
+  }
+  const token = await reply.jwtSign(
+    {
+      id: User.id,
+    },
+    { expiresIn: "2h" }
+  );
+  const result = {
+    name: User.name,
+    id: User.id,
+    token,
+  };
+  reply.send(result);
 };
 
 export const getUser = async (request: FastifyRequest, reply: FastifyReply) => {
   const user = await prismaClient.user.findMany();
   return user;
+};
+export const editProfile = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const user = request.body as Users;
+  const { id } = request.params as Users;
+  const User = await prismaClient.user.findUnique({
+    where: { id: id },
+  });
+
+  return await prismaClient.user.update({
+    where: { id: User.id },
+    data: {
+      ...user,
+    },
+  });
+};
+export const editPassword = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const user = request.body as Users;
+  const { id } = request.params as Users;
+  const User = await prismaClient.user.findUnique({
+    where: { id: id },
+  });
+
+  const checkPassword = await bcrypt.compare(user.oldPassword, User.password);
+  const hashedPassword = await bcrypt.hash(user.password, Number(SALT));
+  return await prismaClient.user.update({
+    where: { id: User.id },
+    data: {
+      password: checkPassword ? hashedPassword : null,
+    },
+  });
 };
