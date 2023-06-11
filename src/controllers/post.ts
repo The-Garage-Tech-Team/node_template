3562ca442af6f3671cbeba3eb0fb1c94fdf82dc2
sultan901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2023-06-11 17:13:42 +0300

    add:file Upload

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -3,20 +3,34 @@ import { prismaClient } from "../prisma";
 import { Post } from "../type-object/post-type";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { Commentes } from "../type-object/commentes-type";
-export const addPost = async (req: FastifyRequest, res: FastifyReply) => {
+import fs from "fs";
+import { timeStamp } from "console";
+export const addPost = async (req: any, res: FastifyReply) => {
   const posts = req.body as Post;
   let authorId = "";
-  const token = req.headers.authorization.split(" ")[1] as Partial<VerifyOptions>;
+  const token = req.headers.authorization.split(
+    " "
+  )[1] as Partial<VerifyOptions>;
 
-   req.jwtVerify(token, (_err, decoded) => {
+  req.jwtVerify(token, (_err, decoded) => {
     authorId = decoded.id;
-    return decoded;  
+    return decoded;
   });
+console.log();
 
+  let files = req.raw.files;
+  let file = files.undefined;
+  let fileName;
+  for (let key in file) {
+    fileName = Date.now() + file[key].name;
+    fs.writeFile(
+      `./uploads/${Date.now()}${file[key].name}`,
+      file[key].data,
+      () => {}
+    );
+  }
   const post = await prismaClient.post.create({
-    data: { ...posts,  authorId },
+    data: { ...posts, authorId, file: fileName },
   });
   res.send(post);
 };
-
-
--- src/routes/post-route.ts
+++ src/routes/post-route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { addPost } from "../controllers/post";
-import { authentication } from '../hooks/auth';
+import { authentication } from "../hooks/auth";
 import { Post } from "../type-object/post-type";
 export default async (server: FastifyInstance) => {
   server.route({
@@ -9,10 +9,16 @@ export default async (server: FastifyInstance) => {
     schema: {
       summary: "Post",
       tags: ["post"],
-      body: Post,
+      body: {
+        type: "object",
+        properties: {
+          file: { type: "object" },
+          description: { type: "string" },
+          authorId: { type: "string" },
+        },
+      },
     },
     onRequest: authentication.authenticate,
     handler: addPost,
   });
-
 };
